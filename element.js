6403b555438e81862ded337a32cf34583a971aad
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-18 15:35:10 +0200

    JSDoc typedef for an object named HtmlElements

---
+++ element.js
@@ -0,0 +1,31 @@
+
+/**
+ * @typedef {Object} HtmlElements
+ * @prop {Object} list
+ * @prop {HTMLElement} list.dataListItem
+ * @prop {HTMLElement} list.dataListButton
+ * @prop {HTMLElement} list.dataListClose
+ * @prop {HTMLElement} list.dataListActive
+ * @prop {HTMLElement} list.dataListMessage
+ * @prop {HTMLElement} list.dataListBlur
+ * @prop {HTMLElement} list.dataListImage
+ * @prop {HTMLElement} list.dataListTitle
+ * @prop {HTMLElement} list.dataListSubtitle
+ * @prop {HTMLElement} list.dataListDescription
+ * @prop {Object} header
+ * @prop {HTMLElement} header.dataHeaderSearch
+ * @prop {HTMLElement} header.dataHeaderSetting
+ * @prop {Object} search
+ * @prop {HTMLElement} search.dataSearchAuthor
+ * @prop {HTMLElement} search.dataSearchCancel
+ * @prop {HTMLElement} search.dataSearchGenre
+ * @prop {HTMLElement} search.dataSearchOverlay
+ * @prop {HTMLElement} search.dataSearchTitle
+ * @prop {HTMLElement} search.dataSearchForm
+ * @prop {Object} setting
+ * @prop {HTMLElement} setting.dataSettingTheme
+ * @prop {HTMLElement} setting.dataSettingCancel
+ * @prop {HTMLElement} setting.dataSettingOverlay
+ * @prop {HTMLElement} setting.dataSettingForm
+
+ */
