f0457e071c9dc0b325e72b611cc0b29ae9b94bb7
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-18 16:06:52 +0200

    adds event listeners to form elements and handles form submission events in the webpage

--- scripts.js
+++ scripts.js
@@ -27,6 +27,38 @@ htmlElements.list.dataListClose.addEventListener('click', () => {
 });
 
 
+htmlElements.setting.dataSettingForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const { theme } = Object.fromEntries(formData);
+
+    setThemeProperties(theme);
+    
+    htmlElements.setting.dataSettingOverlay.open = false;
+});
+
+htmlElements.search.dataSearchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const filters = Object.fromEntries(formData)
+    const result = []
+
+    for (const book of books) {
+        let genreMatch = filters.genre === 'any'
+
+        for (const singleGenre of book.genres) {
+            if (genreMatch) break;
+            if (singleGenre === filters.genre) { genreMatch = true }
+        }
+
+        if (
+            (filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase())) && 
+            (filters.author === 'any' || book.author === filters.author) && 
+            genreMatch
+        ) {
+            result.push(book)
+        }
+    }
 
 
 
