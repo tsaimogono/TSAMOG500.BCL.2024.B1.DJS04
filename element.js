c9deeb39aafaf67d051193de3f4de274b288e880
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-18 15:39:45 +0200

    constant htmlElements and exports it using JavaScript's export.

--- element.js
+++ element.js
@@ -29,3 +29,40 @@
  * @prop {HTMLElement} setting.dataSettingForm
 
  */
+
+
+/**
+ * @type {HtmlElements}
+ */
+export const htmlElements = {
+    list: {
+        dataListItem: document.querySelector('[data-list-items]'),
+        dataListButton: document.querySelector('[data-list-button]'),
+        dataListClose: document.querySelector('[data-list-close]'),
+        dataListActive: document.querySelector('[data-list-active]'),
+        dataListMessage: document.querySelector('[data-list-message]'),
+        dataListBlur: document.querySelector('[data-list-blur]'),
+        dataListImage: document.querySelector('[data-list-image]'),
+        dataListTitle: document.querySelector('[data-list-title]'),
+        dataListSubtitle: document.querySelector('[data-list-subtitle]'),
+        dataListDescription: document.querySelector('[data-list-description]')
+    },
+    header: {
+        dataHeaderSearch: document.querySelector('[data-header-search]'),
+        dataHeaderSetting: document.querySelector('[data-header-settings]')
+    },
+    search: {
+        dataSearchAuthor: document.querySelector('[data-search-authors]'),
+        dataSearchCancel: document.querySelector('[data-search-cancel]'),
+        dataSearchGenre: document.querySelector('[data-search-genres]'),
+        dataSearchOverlay: document.querySelector('[data-search-overlay]'),
+        dataSearchTitle: document.querySelector('[data-search-title]'),
+        dataSearchForm: document.querySelector('[data-search-form]')
+    },
+    setting: {
+        dataSettingTheme: document.querySelector('[data-settings-theme]'),
+        dataSettingCancel: document.querySelector('[data-settings-cancel]'),
+        dataSettingOverlay: document.querySelector('[data-settings-overlay]'),
+        dataSettingForm: document.querySelector('[data-settings-form]')
+    },
+}
